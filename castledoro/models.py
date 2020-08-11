d496de1413cf5d8d90b530786d97ac82b394d9b8
Jessica Biondi <jbiondi.cs@gmail.com>
2020-08-11 13:42:59 -0700

    Custom user model progress

--- castledoro/models.py
+++ castledoro/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=60, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    signup_date = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', ]
