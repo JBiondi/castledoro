bd8fc1dc99ff5955e4912db0ebad008533cdf86a
Jessica Biondi <jbiondi.cs@gmail.com>
2020-08-13 14:56:23 -0700

    Model updates

--- castledoro/models.py
+++ castledoro/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+from users.models import CustomUser
+
+
+class Castle(models.Model):
+    castle_id = models.AutoField(primary_key=True)
+    associated_user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL())
+    castle_name = models.CharField(default='Castle Name', max_length=40)
+    total_blocks = models.IntegerField(default=250)
+    completed_blocks = models.IntegerField(default=0)
+    banner_color = models.CharField(default='blue')
