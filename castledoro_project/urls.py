5e188c6025b1bdd5cd7c29d58aead9cbe75d3603
Jessica Biondi <jbiondi.cs@gmail.com>
2020-08-19 13:41:24 -0700

    User registration form setup

--- castledoro_project/urls.py
+++ castledoro_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from users import views as user_views
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
+    path('register/', user_views.user_registration, name='user_registration_namespace'),
 ]
