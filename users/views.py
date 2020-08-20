acc981690aa6102c12b68a74c39196eaf7e9f737
Jessica Biondi <jbiondi.cs@gmail.com>
2020-08-20 13:35:30 -0700

    User registration setup

--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
 def user_registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('home_namespace')
+
     form = UserCreationForm()
 
     context = {'form': form}
