1c4282dc41f374ac49ef26af8a103dcf4020857d
Jessica Biondi <jbiondi.cs@gmail.com>
2020-08-26 14:31:17 -0700

    Castle model change

--- castledoro/models.py
+++ castledoro/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Castle(models.Model):
     castle_id = models.AutoField(primary_key=True)
-    associated_user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    associated_user = models.ForeignKey(User, on_delete=models.CASCADE)
     castle_name = models.CharField(default='Castle Name', max_length=40)
     total_blocks = models.IntegerField(default=250)
     completed_blocks = models.IntegerField(default=0)
