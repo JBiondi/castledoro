398956c89579fd91575e64c7db2e0b41dd7b0738
Jessica Biondi <jbiondi.cs@gmail.com>
2020-09-10 13:33:28 -0700

    Timer improvements

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,29 @@
 const timerHTML = document.querySelector('.timer')
 
 function timer(minutes) {
-    let hours = '00';
+
     let seconds = 60;
 
     minutes--;
-
-    if (minutes > 60) {
-        hours = 60 / minutes;
-    }
 
     let interval = setInterval(function () {
 
         seconds--;
 
         if (minutes < 10 && seconds > 10) {
-            timerHTML.innerHTML = `${hours} : 0${minutes} : ${seconds}`;
+            timerHTML.innerHTML = `0${minutes} : ${seconds}`;
         }
 
         else if (minutes < 10 && seconds < 10) {
-            timerHTML.innerHTML = `${hours} : 0${minutes} : 0${seconds}`;
+            timerHTML.innerHTML = `0${minutes} : 0${seconds}`;
+        }
+
+        else if (minutes > 10 && seconds < 10) {
+            timerHTML.innerHTML = `${minutes} : 0${seconds}`;
         }
 
         else {
-            timerHTML.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+            timerHTML.innerHTML = `${minutes} : ${seconds}`;
         }
 
         if (!seconds && !minutes) {
@@ -35,9 +35,10 @@ function timer(minutes) {
             minutes--;
             seconds = 60;
         }
+
     }, 1000)
 }
 
 
-timer(2);
+timer(12);
 
