92ef4b6ba53c8335beda3c19bc868ca9e6e31fcd
Jessica Biondi <jbiondi.cs@gmail.com>
2020-09-29 08:05:33 -0700

    Castle model default block change

--- castledoro/models.py
+++ castledoro/models.py
@@ -6,7 +6,7 @@ class Castle(models.Model):
     castle_id = models.AutoField(primary_key=True)
     associated_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     castle_name = models.CharField(default='Castle Name', max_length=40)
-    total_blocks = models.IntegerField(default=250)
+    total_blocks = models.IntegerField(default=25)
     completed_blocks = models.IntegerField(default=0)
     banner_color = models.CharField(default='blue', max_length=25)
     last_modified = models.DateTimeField(auto_now=True)
