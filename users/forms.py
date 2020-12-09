b96974366e82362faf927b8c7cab3bc9cddc9352
Jessica Biondi <jbiondi.cs@gmail.com>
2020-12-09 13:27:11 -0800

    User registration form changes

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(help_text='Optional (only used for password reset)', required=False)
+    username = forms.CharField(help_text='')
+    password1 = forms.CharField(help_text='')
+    password2 = forms.CharField(help_text='')
 
     class Meta:
         model = User
