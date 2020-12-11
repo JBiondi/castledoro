9de9d1faabdabacdf7f89f86cf2e24e001bb1feb
Jessica Biondi <jbiondi.cs@gmail.com>
2020-12-11 11:32:23 -0800

    Registration form improvements

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(help_text='Optional (used for password reset)', required=False)
-    username = forms.CharField(help_text='')
+    email = forms.EmailField(help_text='', required=False, label='email (optional)')
+
+    username = forms.CharField(help_text='', label='username', max_length=18)
     username.widget.attrs.update({'class': 'registration-form-username'})
-    password1 = forms.CharField(help_text='')
-    password2 = forms.CharField(help_text='')
+
+    password1 = forms.CharField(help_text='', label='password')
+    password2 = forms.CharField(help_text='', label='re-type password')
 
     class Meta:
         model = User
