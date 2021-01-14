e4da8914e787fc73f132156ce200150f2281a946
Jessica Biondi <jbiondi.cs@gmail.com>
2021-01-14 13:13:36 -0800

    Preparations for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn castledoro_project.wsgi
--- castledoro_project/settings.py
+++ castledoro_project/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'user_profile_namespace'
